8493617a2f440bd0ef3e284521a92713bf6ba5a1
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-06-22 09:28:36 +0530

    Create question03.cpp

---
+++ 01) Arrays/question03.cpp
@@ -0,0 +1,27 @@
+/*
+💡 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+**Example 1:**
+Input: nums = [1,3,5,6], target = 5
+*/
+
+
+int func(vector<int>& nums, int target) {
+    int start=0,end = nums.size()-1;
+
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[mid] > target){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+    return start;
+}
