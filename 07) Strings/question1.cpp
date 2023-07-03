2e2df547db1e41d11bb81e945e7b94053109ea83
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-07-03 22:27:09 +0530

    Create question1.cpp

---
+++ 07) Strings/question1.cpp
@@ -0,0 +1,26 @@
+/*
+Given two strings s and t, *determine if they are isomorphic*.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+**Example 1:**
+
+**Input:** s = "egg", t = "add"
+
+**Output:** true
+*/
+
+bool func(string s, string t){ 
+    int l1=s.length();
+    int chk1[256]={},chk2[256]={};
+
+    for(int i=0;i<l1;i++){
+        if(chk1[s[i]] != chk2[t[i]])
+            return false;
+        chk1[s[i]] = chk2[t[i]]= i+1;
+        
+    }
+    return true;
+}
